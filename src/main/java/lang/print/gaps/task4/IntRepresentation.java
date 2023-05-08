0626c45468a6278edabcd7bfc6b5edb48f8b384b
Fanis <mrduck597@gmail.com>
2023-05-08 20:14:40 +0500

    for test

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,15 +2,15 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int c = (char) 'c';
-        int h = (char) 'h';
-        int a = (char) 'a';
-        int r = (char) 'r';
+        int c;
+        int h;
+        int a;
+        int r;
 
-        System.out.println(c);
-        System.out.println(h);
-        System.out.println(a);
-        System.out.println(r);
+        System.out.println(c = (char) 'c');
+        System.out.println(h = (char) 'h');
+        System.out.println(a = (char) 'a');
+        System.out.println(r  = (char) 'r');
 
     }
 }
